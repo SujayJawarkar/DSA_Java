153995070cdfa856f422fb847fc3e5e27a2539c5
Sujay Jawarkar <sujayp175@gmail.com>
2025-07-23 00:47:10 +0530

    Array - MinDiff & MaxAbsoluteDiff

--- src/arrays/MinMaxDifferenceArray.java
+++ src/arrays/MinMaxDifferenceArray.java
@@ -7,10 +7,43 @@ import java.util.List;
 public class MinMaxDifferenceArray {
     public static void main(String[] args) {
         int[] a = {12,8,9,2,11};
-        List<List<Integer>> ans = minimumDiff(a);
-        System.out.println(ans);
+//        List<List<Integer>> ans = minimumDiff(a);
+        System.out.println(maxDiff(a));
 
     }
+
+    static int maxDiff(int[] a){
+        int n = a.length;
+        int[] aipi =  new int[n];
+        int[] aimi =  new int[n];
+
+        for(int i = 0; i < n; i++){
+            aipi[i] = a[i] + i;
+            aimi[i] = a[i] - i;
+        }
+
+        int minaipi =  Integer.MAX_VALUE;
+        int maxaipi =  Integer.MIN_VALUE;
+
+        int minaimi =  Integer.MAX_VALUE;
+        int maxaimi =  Integer.MIN_VALUE;
+
+        for(int e: aipi){
+            minaipi = Math.min(minaipi,e);
+            maxaipi = Math.max(maxaipi,e);
+        }
+        for(int e: aimi){
+            minaimi = Math.min(minaimi,e);
+            maxaimi = Math.max(maxaimi,e);
+        }
+
+        int diffaipi = maxaipi - minaipi;
+        int difaimi = maxaimi - minaimi;
+
+        return Math.max(diffaipi, difaimi);
+
+    }
+
     static List<List<Integer>> minimumDiff(int[] a){
         List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(a);
